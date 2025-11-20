b5eab156bdb71eafffd6873612da998f765a5b00
Armaldo <armando.v.mohr@gmail.com>
2025-11-20 10:50:56 -0300

    cambios modelos y dashboard

--- Productos/models.py
+++ Productos/models.py
@@ -6,12 +6,12 @@ UNIT_CHOICES = [
     ('kg', 'kg'),
 ]
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
     def __str__(self):
         return self.nombre
 
 class Proveedor(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
     telefono = models.CharField(max_length=50)
     email = models.EmailField(max_length=254) 
     direccion = models.CharField(max_length=50)
