e66e10232d23d8ab2d76270db1d1e998a77dca9a
Javier C. <nemu7945@gamil.com>
2025-11-24 19:09:39 -0300

    movimiento salida

--- Movimientos/views.py
+++ Movimientos/views.py
@@ -5,9 +5,11 @@ from Productos.models import Producto
 
 def listado_movimientos(request):
     Movimientos_Ingreso = Ingreso.objects.all()
+    Movimientos_Salida = Salida.objects.all()
     return render(request, "movimientos/listado_movimientos.html", {
         "titulo":"Listado de Movimientos",
-        "Movimientos": Movimientos_Ingreso
+        "Movimientos_Ingreso": Movimientos_Ingreso,
+        "Movimientos_Salida": Movimientos_Salida
     })
 
 def ingreso(request, ingreso_id=None):
@@ -105,12 +107,91 @@ def movimiento_ingreso(request, ingreso_id):
 
 
 def salida(request):
-    return render(request, "movimientos/salida.html")
+    # Inicializar datos temporales de movimientos en sesión
+    if 'movimientos_salida_temp' not in request.session:
+        request.session['movimientos_salida_temp'] = []
+
+    movimientos_salida_temp = request.session.get('movimientos_salida_temp', [])
+
+    if request.method == 'POST':
+        if 'guardar_salida' in request.POST:
+            salida_id = request.session.get('salida_id')
+            if salida_id:
+                salida = Salida.objects.get(id=salida_id)
+            else:
+                return redirect('salida')
+            
+            for mov in movimientos_salida_temp:
+                movimiento = MovimientoSalida()
+                movimiento.salida = salida
+                movimiento.producto_id = mov['producto_id']
+                movimiento.cantidad = mov['cantidad']
+                movimiento.save()
+                # Actualizar stock Producto
+                producto = Producto.objects.get(id=mov['producto_id'])
+                producto.stock -= mov['cantidad']
+                producto.save()
+
+            # Limpiar session
+            request.session['movimientos_salida_temp'] = []
+            request.session['salida_id'] = None
+            return redirect('listado_movimientos')
+
+        elif 'guardar_datos_salida' in request.POST:
+            form_salida = SalidaForm(request.POST)
+            if form_salida.is_valid():
+                salida = form_salida.save()
+                request.session['salida_id'] = salida.id
+                request.session.modified = True
+                return redirect('movimiento_salida')
+        else:
+            form_salida = SalidaForm()
+    else:
+        salida_id = request.session.get('salida_id')
+        if salida_id:
+            try:
+                salida_instance = Salida.objects.get(id=salida_id)
+                form_salida = SalidaForm(instance=salida_instance)
+            except Salida.DoesNotExist:
+                form_salida = SalidaForm()
+        else:
+            form_salida = SalidaForm()
+
+    return render(request, "movimientos/salidas/salida.html", {
+        'form_salida': form_salida,
+        'movimientos': movimientos_salida_temp,
+        'salida_id': request.session.get('salida_id'),
+    })
 
 def movimiento_salida(request):
-    return render(request, "movimientos/movimiento_salida.html")
+    if 'movimientos_salida_temp' not in request.session:
+        request.session['movimientos_salida_temp'] = []
+    else:
+        movimientos_salida_temp = request.session.get('movimientos_salida_temp', [])
+        request.session['movimientos_salida_temp'] = movimientos_salida_temp
+        request.session.modified = True
 
-def detalle_movimiento(request, ingreso_id):
+    if request.method == 'POST':
+        form_movimiento = MovimientoSalidaForm(request.POST)
+        if form_movimiento.is_valid():
+            movimiento_data = {
+                'producto_id': form_movimiento.cleaned_data['producto'].id,
+                'producto_nombre': str(form_movimiento.cleaned_data['producto']),
+                'cantidad': float(form_movimiento.cleaned_data['cantidad']),
+            }
+            movimientos_salida_temp = request.session.get('movimientos_salida_temp', [])
+            movimientos_salida_temp.append(movimiento_data)
+            request.session['movimientos_salida_temp'] = movimientos_salida_temp
+            request.session.modified = True
+            return redirect('salida')
+    else:
+        form_movimiento = MovimientoSalidaForm()
+
+    return render(request, "movimientos/salidas/movimiento_salida.html", {
+        'form_movimiento': form_movimiento,
+    })
+
+def detalle_movimiento_ingreso(request, ingreso_id):
     ingreso = get_object_or_404(Ingreso, id=ingreso_id)
     movimientos_qs = ingreso.movimientoingreso_set.all()
     movimientos = []
@@ -126,3 +207,19 @@ def detalle_movimiento(request, ingreso_id):
         "ingreso": ingreso,
         "movimientos": movimientos
     })
+
+def detalle_movimiento_salida(request, salida_id):
+    salida = get_object_or_404(Salida, id=salida_id)
+    movimientos_qs = salida.movimientosalida_set.all()
+    movimientos = []
+    for movimiento in movimientos_qs:
+        mov_dict = {
+            'producto': movimiento.producto,
+            'cantidad': movimiento.cantidad,
+        }
+        movimientos.append(mov_dict)
+    return render(request, "movimientos/salidas/detalle_movimiento_salida.html", {
+        "salida": salida,
+        "movimientos": movimientos
+    })
+
