9c2e5e84d892888a170b3a06228103213648bf2e
Armaldo <armando.v.mohr@gmail.com>
2025-11-26 20:37:06 -0300

    Ahora si

--- reportes/urls.py
+++ reportes/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('export_inventory', export_inventory, name='export_inventory'),
+    path('listado_reportes', listado_reportes, name='listado_reportes'),
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -3,8 +3,30 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from Productos.models import Producto  # Importamos el modelo de donde sacamos los datos
 from .models import Reporte
+import datetime
 # Create your views here.
+
+
+
+
+def listado_reportes(request):
+    reportes = Reporte.objects.all()
+    return render(request, 'reportes/listado_reportes.html', {'reportes': reportes})
 def export_inventory(request):
+    # verificar que el usuario este logueado
+    if not request.session.get('user_id'):
+        return redirect('login')
+
+    # verificar que el usuario tenga permisos
+    if not 9 in request.session.get('permisos'):
+        return redirect('inicio')
+    request.session['user_id'] = request.user.id
+    fecha_actual = datetime.datetime.now()
+    reporte = Reporte.objects.create(
+        nombre='Reporte de Stock',
+        fecha=fecha_actual,
+        usuario_id=request.session['user_id']
+    )
     workbook = openpyxl.Workbook()
     worksheet = workbook.active
     worksheet.title = 'Stock de Productos'
@@ -42,7 +64,6 @@ def export_inventory(request):
     )
     # Definimos el nombre del archivo que se descargará
     response['Content-Disposition'] = 'attachment; filename="reporte_stock.xlsx"'
-
     #Guardar el libro en la respuesta
     workbook.save(response)
     
